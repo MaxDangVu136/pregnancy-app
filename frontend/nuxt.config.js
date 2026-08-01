3c409665c17c477db9f8e30718aa3c884208e7e5
Max Dang Vu <mdan066@aucklanduni.ac.nz>
2026-08-01 22:52:28 +1200

    test: marker.io screenshots

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -75,7 +75,7 @@ export default {
       {
         hid: "marker-io",
         innerHTML:
-          "window.markerConfig={project:'6a6c1e67f805db9540faf408',source:'snippet'};!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};['show','hide','isVisible','capture','cancelCapture','unload','reload','isExtensionInstalled','setReporter','clearReporter','setCustomData','on','off'].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement('script');s.async=1,s.src='https://edge.marker.io/latest/shim.js';var i=r.getElementsByTagName('script')[0];i.parentNode.insertBefore(s,i)}}(window,document);",
+          "window.markerConfig={project:'6a6c1e67f805db9540faf408',source:'snippet',useNativeScreenshot:true};!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};['show','hide','isVisible','capture','cancelCapture','unload','reload','isExtensionInstalled','setReporter','clearReporter','setCustomData','on','off'].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement('script');s.async=1,s.src='https://edge.marker.io/latest/shim.js';var i=r.getElementsByTagName('script')[0];i.parentNode.insertBefore(s,i)}}(window,document);",
         type: "text/javascript",
       },
     ],
